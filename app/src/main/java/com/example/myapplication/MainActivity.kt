558cbba6ef0e7b460f08b6d1c20ec8af49f7ed9d
karinasouadki <karina.souadki@gmail.com>
2021-11-24 19:16:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.StyleSpan
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.product_barcode).setBoldValue(
+            getString(R.string.product_barcode, "1234567")
+        )
+    }
+
+    fun TextView.setBoldValue(value: String) {
+        val index = value.indexOf(':')
+        val spannable = SpannableString(value)
+
+        spannable.setSpan(StyleSpan(Typeface.BOLD), 0, index, 0)
+        setText(spannable)
+    }
+}
